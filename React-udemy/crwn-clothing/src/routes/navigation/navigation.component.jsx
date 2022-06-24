762518d940e3348822a8f6e35fd577868b4f67d3
Yihang Lu <yihang.lu@zendesk.com>
2022-06-23 21:56:11 -0500

    add shopping page and cart function

--- React-udemy/crwn-clothing/src/routes/navigation/navigation.component.jsx
+++ React-udemy/crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -4,11 +4,16 @@ import { Link, Outlet } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from "../../context/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import { CartContext } from "../../context/cart.context";
+
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
@@ -25,7 +30,9 @@ const Navigation = () => {
               <Link className="nav-link" to='/auth'>SIGN IN</Link>
             )
           }
+          <CartIcon />
         </div>
+        { isCartOpen && <CartDropdown /> }
       </div>
       <Outlet />
     </Fragment>
